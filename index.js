c628e51bfce88dbf05accd2b0dd97b4ff7e3cb34
Ouyang Yadong <oyydoibh@gmail.com>
2021-12-08 02:23:32 +0800

    fix: fix addon name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { loadBinding } = require('@node-rs/helper')
 
-const bindings = loadBinding(__dirname, 'package-template', 'rs-object')
+const bindings = loadBinding(__dirname, 'rs-object', 'rs-object')
 
 const File = bindings.createFileClass()
 
